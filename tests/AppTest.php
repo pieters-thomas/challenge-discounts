bc4f92ae47ee99be6dd9c980cad87e22efc48949
Thomas Pieters <pieters.tg@gmail.com>
2021-07-08 09:47:46 +0200

    Apptest checks values

--- tests/AppTest.php
+++ tests/AppTest.php
@@ -14,17 +14,31 @@ class AppTest extends TestCase
     /**
      * @throws \JsonException
      */
-    public function testApplication(): void
+    public function dataProviderApplication(): array
     {
         $api = new OrderApi();
-        $order = $api->fetchOrderById(1);
+
+        return [
+            [$api->fetchOrderById(1),49.90,49.90],
+            [$api->fetchOrderById(2),24.95,22.46],
+            [$api->fetchOrderById(3),69.00,67.05],
+        ];
+    }
+
+    /**
+     * @dataProvider dataProviderApplication
+     */
+    public function testApplication(Order $order, $rawTotal, $discountTotal): void
+    {
 
         self::assertInstanceOf(Order::class, $order);
+
+        self::assertEquals(number_format($order->getTotal()->getAmount(),2), $rawTotal);
 
         $discountManager = new DiscountManager();
         $discountManager->applyDiscounts($order);
 
-        self::assertInstanceOf(Order::class, $order);
-        //TODO write test that checks the outcomes for each order;
+        self::assertEquals(number_format($order->getTotal()->getAmount(),2) , $discountTotal);
+
 }
 }
